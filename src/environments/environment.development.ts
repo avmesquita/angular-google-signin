708dc153dcb24af63920ee407487f57ade02da65
Andre Mesquita <avmesquita@gmail.com>
2024-08-25 03:20:50 +0100

    test

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    googleClientID: '796854264817-16m92ptkdh0lbundqm3rln5i41vie3e7.apps.googleusercontent.com'
+    googleClientID: '796985264817-16m92ptkdh0lbundqm3rln5i41454e3e7.apps.googleusercontent.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    googleClientID: '796854264817-16m92ptkdh0lbundqm3rln5i41vie3e7.apps.googleusercontent.com'
+    googleClientID: '796985264817-16m92ptkdh0lbundqm3rln5i41454e3e7.apps.googleusercontent.com'
 };
 
